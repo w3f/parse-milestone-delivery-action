6debdf06f1870721d84b6881455297799506b53c
Matteo Casonato <matteocasonato@protonmail.com>
2023-02-03 10:13:17 +0100

    Improve checks
    - improve checks
    - fix CI

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const main = async () => {
     {
       name: 'application_document',
       regex: /(?<=\*\*Application Document:\*\* ).*/g,
+      url_regex: /(https?:\/\/(www\.)?github\.com\/w3f\/(Open-)?Grants-Program\/(blob|tree)\/master\/(applications|maintenance)\/)([-a-zA-Z0-9():_.~! ]|%[0-9a-fA-F]{2})+.md/g,
+      filename_regex: /(?<=\/)([-a-zA-Z0-9():_.~! ]|%[0-9a-fA-F]{2})+.md/g
     },
     {
       name: 'milestone_number',
-      regex: /(?<=\*\*Milestone Number:\*\* ).*/g,
+      regex: /(?<=\*\*(Milestone|Delivery) Number:\*\* ).*/g
     },
   ]
 
-  let not_found = []
+  let errors = []
   let isMaintenance = false
 
   regexList.map(function (reg) {
@@ -26,22 +28,22 @@ const main = async () => {
       switch (reg.name) {
         case 'application_document':
           let application_document = content.match(reg.regex)[0]
-          const application_document_regex = /(https?:\/\/(www\.)?github\.com\/w3f\/(Open-)?Grants-Program\/(blob|tree)\/master\/(applications|maintenance)\/)([-a-zA-Z0-9():_.~! ]|%[0-9a-fA-F]{2})+.md/g
-          if (application_document.match(application_document_regex)) {
-            isMaintenance = application_document.includes('maintenance')
-            application_document = application_document.split('/').pop()
+          application_document = application_document.match(reg.url_regex)[0]
+          isMaintenance = application_document.includes('maintenance')
+          application_document = application_document.match(reg.filename_regex)[0]
+          if (application_document) {
             core.setOutput(reg.name, application_document)
             core.setOutput('is_maintenance', isMaintenance)
           } else {
-            not_found.push(reg.name)
+            errors.push(reg.name)
           }
           break
 
         case 'milestone_number':
-          if (isMaintenance) {
-            core.setOutput(reg.name, 0)
+          const milestone_number = content.match(reg.regex)[0]
+          if (isNaN(milestone_number)) {
+            errors.push(reg.name)
           } else {
-            const milestone_number = content.match(reg.regex)[0]
             core.setOutput(reg.name, milestone_number)
           }
           break
@@ -51,12 +53,12 @@ const main = async () => {
           core.setOutput(reg.name, result)
       }
     } catch {
-      not_found.push(reg.name)
+      errors.push(reg.name)
     }
   })
   
-  if (not_found.length > 0) {
-    const error_string = `Match not found for: ${not_found.join(', ')}`
+  if (errors.length > 0) {
+    const error_string = `Match not found for: ${errors.join(', ')}`
     core.setFailed(error_string)
   }
 }
